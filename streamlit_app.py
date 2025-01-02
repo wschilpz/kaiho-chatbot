eb8231a9021859f0ed1879311830d0c5430fe806
wschilpz <warren.schilpzand@gmail.com>
2025-01-02 02:23:30 +0000

    second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,9 @@ TWEAKS = {
 }
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Kaiho Logistics Agent")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Logistics agent that can answer questions based on the logistics agreements for your company."
 )
 
 def run_flow(message: str,
